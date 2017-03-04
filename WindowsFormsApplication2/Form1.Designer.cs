fcbf97d932a5b3efec350910206fc19d05324867
eemilyjiang <emilyjiangzju@163.com>
2017-03-04 18:55:07 +0800

    messagebox0304

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.combutton1 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 367);
+            this.panel1.TabIndex = 0;
+            // 
+            // combutton1
+            // 
+            this.combutton1.Location = new System.Drawing.Point(46, 399);
+            this.combutton1.Name = "combutton1";
+            this.combutton1.Size = new System.Drawing.Size(75, 23);
+            this.combutton1.TabIndex = 1;
+            this.combutton1.Text = "connect";
+            this.combutton1.UseVisualStyleBackColor = true;
+            this.combutton1.Click += new System.EventHandler(this.combutton1_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 507);
+            this.Controls.Add(this.combutton1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+       
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button combutton1;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ozeki.Media;
+using Ozeki.Camera;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private IIPCamera _camera;
+        private DrawingImageProvider _imageProvider = new DrawingImageProvider();
+        private MediaConnector _connector = new MediaConnector();
+        private VideoViewerWF _videoViewerWF1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int noone;
+            float nnnn;
+            // Create video viewer UI control
+            _videoViewerWF1 = new VideoViewerWF();
+            _videoViewerWF1.Name = "videoViewerWF1";
+            _videoViewerWF1.Size = panel1.Size;
+            panel1.Controls.Add(_videoViewerWF1);
+
+            // Bind the camera image to the UI control
+            _videoViewerWF1.SetImageProvider(_imageProvider);
+        }
+
+        private void combutton1_Click(object sender, EventArgs e)
+        {   //输入
+            _camera = IPCameraFactory.GetCamera("rtsp://192.168.113.150:554/ufirststream", "root", "pass");
+            _connector.Connect(_camera.VideoChannel, _imageProvider);
+            _camera.Start();
+            _videoViewerWF1.Start();
+
+        }
+    }
+}
